b0de1a0082e6f4451eec09f35161dd41423372c1
GIHAN SHAMIKE <gihanzshamike@gmail.com>
2024-12-08 09:56:17 -0500

    Sampath LC Commit

---
+++ src/com/sampath/sagt/eventaction/util/FileNetConstants.java
@@ -0,0 +1,12 @@
+package com.sampath.sagt.eventaction.util;
+
+public class FileNetConstants {
+
+	
+	public static String REFERENCE_NO ="MEDI_ReferenceNumber";
+	public static String LC_CASE_TYPE = "LOC_LetterOfCredit";
+	public static String LC_AMENDMENT_CASE_TYPE = "LOC_LCAmendment";
+	public static String SAGT_NEW_REQ = "MEDI_NewRecruitment";
+	public static String SAGT_PROBATION = "MEDI_Probation";
+	public static String SAGT_VMOVISIT = "MEDI_VMOVisiting";
+}
